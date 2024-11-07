787eef8a3226c6eeb6b66f85a99f88972c34289f
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-07 22:37:15 +0900

    Feat : 팔로워 계정명 얻기

--- crawling_minseo.py
+++ crawling_minseo.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import time, os
 from dotenv import load_dotenv
 import pandas as pd
@@ -15,10 +18,12 @@ selector = {
     'cover': 'a ._aagu ._aagv img',
     'text': 'h1._ap3a._aaco._aacu._aacx._aad7._aade',
     'like': 'span a span .html-span.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1hl2dhg.x16tdsg8.x1vvkbs',
-    'date': '.x1p4m5qa'
+    'date': '.x1p4m5qa',
+    'follower': '.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div:nth-child(2) > div > div.x1gryazu.xh8yej3.x10o80wk.x14k21rp.x17snn68.x6osk4m.x1porb0y.x8vgawa > section > main > div > header > section.xc3tme8.x18wylqe.x1xdureb.xvxrpd7.x13vxnyz > ul > li:nth-child(2) > div > a',
+    'follower_account': '.xyi19xy.x1ccrb07.xtf3nb5.x1pc53ja.x1lliihq.x1iyjqo2.xs83m0k.xz65tgg.x1rife3k.x1n2onr6 > div:nth-child(2) > div > div > div > div > div > div.x9f619.x1ja2u2z.x78zum5.x1n2onr6.x1iyjqo2.xs83m0k.xeuugli.x1qughib.x6s0dn4.x1a02dak.x1q0g3np.xdl72j9 > div > div > div > div > div > a > div > div > span'
 }
 
-account_name_list = ['nike', 'chanelofficial']
+account_name_list = ['nike']
 
 load_dotenv(verbose=True)
 INSTAGRAM_ID = os.getenv('INSTAGRAM_ID')
@@ -59,21 +64,27 @@ data = {
     'date': date
 }
 
+follower_data = {
+    'follower': []
+}
+
 # account_name에 접속하여 최근 10개의 posts에서
 # 1. 업로드 날짜
 # 2. 본문 내용
 # 3. 첫번째 이미지
 # 4. 좋아요 개수
+# 5. 팔로워 계정명들
+MAX_POST_lENGTH = 20
+
 for account_name in account_name_list:
     driver.get(f'https://instagram.com/{account_name}/')
     driver.implicitly_wait(10)
 
-    images = []
     covers = driver.find_elements(By.CSS_SELECTOR, selector['cover'])
-    cover_count = len(covers)
-    for i in range(min(10, cover_count)):  # 커버 수가 10개 미만일 경우 대비
-        images.append(covers[i].get_attribute('src'))
-    image.extend(images)
+    MIN_POST_LENGTH = min(MAX_POST_lENGTH, len(covers))
+
+    for i in range(MIN_POST_LENGTH):
+        image.append(covers[i].get_attribute('src'))
 
     # 첫 번째 게시물 클릭
     posts = driver.find_elements(By.CSS_SELECTOR, selector['first_post'])
@@ -86,7 +97,7 @@ for account_name in account_name_list:
         continue
 
     # 게시물에서 본문/좋아요수/날짜 얻기
-    for i in range(10):
+    for i in range(MIN_POST_LENGTH):
         try:
             text.append(driver.find_element(By.CSS_SELECTOR, selector['text']).text)
         except NoSuchElementException:  # 본문이 없는 게시물 예외 처리
@@ -97,16 +108,49 @@ for account_name in account_name_list:
         like.append(driver.find_element(By.CSS_SELECTOR, selector['like']).text)
         date.append(driver.find_element(By.CSS_SELECTOR, selector['date']).get_attribute('title'))
 
-        if i < 9:
+        if i < MIN_POST_LENGTH - 1:
             next_btn = driver.find_element(By.CSS_SELECTOR, selector['next_btn'])
             next_btn.click()
             driver.implicitly_wait(10)
 
-data.update({'image': image})
+    # 팔로워 이름들 얻기
+    ## 1. 계정 홈으로 돌아감
+    driver.get(f'https://instagram.com/{account_name}/')
+
+    ## 2. 팔로워 버튼 눌러 찾기
+    follower_btn = driver.find_element(By.CSS_SELECTOR, selector['follower'])
+    follower_btn.click()
+    driver.implicitly_wait(10)
+
+    ## 3. 팔로워 목록 창 스크롤
+    scrollable_popup = driver.find_element(By.XPATH,
+                                           "//div[@class='x1dm5mii x16mil14 xiojian x1yutycm x1lliihq x193iq5w xh8yej3']")
+    for _ in range(10):
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_popup)
+        time.sleep(2)
+
+    ## 4. 팔로워 계정 이름 가져오기
+    follower_accounts = driver.find_elements(By.CSS_SELECTOR, selector['follower_account'])
+    num_followers = min(100, len(follower_accounts))
+
+    for i in range(num_followers):
+        try:
+            follower_account_name = follower_accounts[i].text
+            follower_data['follower'].append(follower_account_name)
+        except NoSuchElementException:
+            print(f"Follower {i + 1} not found.")
+            break
+
+
 
 df = pd.DataFrame(data)
-df.set_index(keys=['date'], inplace=True, drop=True)
 df.to_excel('./instagram.xlsx')
 
+print("============================================")
 print(df)
-time.sleep(10)
+
+df = pd.DataFrame(follower_data)
+df.to_excel('./instagram_follower.xlsx')
+
+print("============================================")
+print(df)
