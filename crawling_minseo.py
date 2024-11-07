d938364f4202903bf585625e5c8eb42dd98abba8
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-08 02:48:16 +0900

    Feat : 댓글 데이터를 포함하여 JSON 으로 반환

--- crawling_minseo.py
+++ crawling_minseo.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time, os
+import time
+import os
+import json
 from dotenv import load_dotenv
 import pandas as pd
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-
 
 selector = {
     'id_input': '._aa4b._add6._ac4d._ap35',
@@ -19,8 +19,7 @@ selector = {
     'text': 'h1._ap3a._aaco._aacu._aacx._aad7._aade',
     'like': 'span a span .html-span.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1hl2dhg.x16tdsg8.x1vvkbs',
     'date': '.x1p4m5qa',
-    'follower': '.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div:nth-child(2) > div > div.x1gryazu.xh8yej3.x10o80wk.x14k21rp.x17snn68.x6osk4m.x1porb0y.x8vgawa > section > main > div > header > section.xc3tme8.x18wylqe.x1xdureb.xvxrpd7.x13vxnyz > ul > li:nth-child(2) > div > a',
-    'follower_account': '.xyi19xy.x1ccrb07.xtf3nb5.x1pc53ja.x1lliihq.x1iyjqo2.xs83m0k.xz65tgg.x1rife3k.x1n2onr6 > div:nth-child(2) > div > div > div > div > div > div.x9f619.x1ja2u2z.x78zum5.x1n2onr6.x1iyjqo2.xs83m0k.xeuugli.x1qughib.x6s0dn4.x1a02dak.x1q0g3np.xdl72j9 > div > div > div > div > div > a > div > div > span'
+    'comment_span': 'body > div.x14dbnvc.x67yw2k.x14gcc7h.x1xb1xrg.xz3gdfk.xbi9o00.x1xkblxv.x4666fc.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe.x1qjc9v5.xjbqb8w.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xr1yuqi.xkrivgy.x4ii5y1.x1gryazu.x15h9jz8.x47corl.xh8yej3.xir0mxb.x1juhsu6 > div > article > div > div.x1qjc9v5.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x78zum5.xdt5ytf.x1iyjqo2.x5wqa0o.xln7xf2.xk390pu.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x65f84u.x1vq45kp.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x11njtxf > div > div > div.x78zum5.xdt5ytf.x1q2y9iw.x1n2onr6.xh8yej3.x9f619.x1iyjqo2.x18l3tf1.x26u7qi.xy80clv.xexx8yu.x4uap5.x18d9i69.xkhd6sd > div.x78zum5.xdt5ytf.x1iyjqo2.xs83m0k.x2lwn1j.x1odjw0f.x1n2onr6.x9ek82g.x6ikm8r.xdj266r.x11i5rnm.x4ii5y1.x1mh8g0r.xexx8yu.x1pi30zi.x18d9i69.x1swvt13 > ul > div:nth-child(3) > div > div > div > ul > div > li > div > div > div._a9zr > div._a9zs > span'
 }
 
 account_name_list = ['nike']
@@ -52,39 +51,24 @@ el[1].send_keys(INSTAGRAM_PASSWORD)
 el[1].send_keys(Keys.ENTER)
 time.sleep(10)
 
-text = []
-image = []
-like = []
-date = []
+# 데이터를 저장할 리스트 초기화
+data = []
 
-data = {
-    'text': text,
-    'image': image,
-    'like': like,
-    'date': date
-}
-
-follower_data = {
-    'follower': []
-}
-
-# account_name에 접속하여 최근 10개의 posts에서
+# account_name에 접속하여 posts에서
 # 1. 업로드 날짜
 # 2. 본문 내용
 # 3. 첫번째 이미지
 # 4. 좋아요 개수
-# 5. 팔로워 계정명들
-MAX_POST_lENGTH = 20
+# 5. 댓글
+
+MAX_POST_LENGTH = 20
 
 for account_name in account_name_list:
     driver.get(f'https://instagram.com/{account_name}/')
     driver.implicitly_wait(10)
 
     covers = driver.find_elements(By.CSS_SELECTOR, selector['cover'])
-    MIN_POST_LENGTH = min(MAX_POST_lENGTH, len(covers))
-
-    for i in range(MIN_POST_LENGTH):
-        image.append(covers[i].get_attribute('src'))
+    MIN_POST_LENGTH = min(MAX_POST_LENGTH, len(covers))
 
     # 첫 번째 게시물 클릭
     posts = driver.find_elements(By.CSS_SELECTOR, selector['first_post'])
@@ -93,64 +77,55 @@ for account_name in account_name_list:
         post.click()
         driver.implicitly_wait(10)
     else:
-        print(f"No posts found for brand: {account_name}")
+        print(f"No posts found for account: {account_name}")
         continue
 
-    # 게시물에서 본문/좋아요수/날짜 얻기
+    # 게시물에서 본문/좋아요수/날짜/댓글을 얻기
     for i in range(MIN_POST_LENGTH):
+        post_data = {}
+
         try:
-            text.append(driver.find_element(By.CSS_SELECTOR, selector['text']).text)
-        except NoSuchElementException:  # 본문이 없는 게시물 예외 처리
-            text.append('')
-        count = driver.find_element(By.CSS_SELECTOR, selector['like']).text
-        if count == '':
-            time.sleep(1)
-        like.append(driver.find_element(By.CSS_SELECTOR, selector['like']).text)
-        date.append(driver.find_element(By.CSS_SELECTOR, selector['date']).get_attribute('title'))
+            post_data["text"] = driver.find_element(By.CSS_SELECTOR, selector['text']).text
+        except NoSuchElementException:
+            post_data["text"] = ''
+
+        post_data["image"] = covers[i].get_attribute('src')
+
+        try:
+            post_data["like"] = driver.find_element(By.CSS_SELECTOR, selector['like']).text
+        except NoSuchElementException:
+            post_data["like"] = '0'
+
+        try:
+            post_data["date"] = driver.find_element(By.CSS_SELECTOR, selector['date']).get_attribute('title')
+        except NoSuchElementException:
+            post_data["date"] = ''
+
+        # 댓글 수집
+        post_data["comment"] = []
+        comments = driver.find_elements(By.CSS_SELECTOR, selector['comment_span'])
+
+        for comment in comments:
+            try:
+                post_data["comment"].append(comment.text)
+            except NoSuchElementException:
+                break
+
+        data.append(post_data)
 
         if i < MIN_POST_LENGTH - 1:
-            next_btn = driver.find_element(By.CSS_SELECTOR, selector['next_btn'])
-            next_btn.click()
-            driver.implicitly_wait(10)
+            click_nxt()
 
-    # 팔로워 이름들 얻기
-    ## 1. 계정 홈으로 돌아감
-    driver.get(f'https://instagram.com/{account_name}/')
+# JSON 파일로 저장
+with open('./instagram.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
 
-    ## 2. 팔로워 버튼 눌러 찾기
-    follower_btn = driver.find_element(By.CSS_SELECTOR, selector['follower'])
-    follower_btn.click()
-    driver.implicitly_wait(10)
+print("==============================")
 
-    ## 3. 팔로워 목록 창 스크롤
-    scrollable_popup = driver.find_element(By.XPATH,
-                                           "//div[@class='x1dm5mii x16mil14 xiojian x1yutycm x1lliihq x193iq5w xh8yej3']")
-    for _ in range(10):
-        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_popup)
-        time.sleep(2)
+with open('./instagram.json', 'r', encoding='utf-8') as f:
+    json_data = json.load(f)
 
-    ## 4. 팔로워 계정 이름 가져오기
-    follower_accounts = driver.find_elements(By.CSS_SELECTOR, selector['follower_account'])
-    num_followers = min(100, len(follower_accounts))
+print(json.dumps(json_data, ensure_ascii=False, indent=4))
 
-    for i in range(num_followers):
-        try:
-            follower_account_name = follower_accounts[i].text
-            follower_data['follower'].append(follower_account_name)
-        except NoSuchElementException:
-            print(f"Follower {i + 1} not found.")
-            break
-
-
-
-df = pd.DataFrame(data)
-df.to_excel('./instagram.xlsx')
-
-print("============================================")
-print(df)
-
-df = pd.DataFrame(follower_data)
-df.to_excel('./instagram_follower.xlsx')
-
-print("============================================")
-print(df)
+# 드라이버 종료
+driver.quit()
