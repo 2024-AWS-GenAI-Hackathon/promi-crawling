6ccdbaac31cc45dd77c40426c3e476c2ebea9cd3
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-08 22:45:31 +0900

    Feat : category_XPATH 설정

--- crawling_naver.py
+++ crawling_naver.py
@@ -14,7 +14,9 @@ from selenium.webdriver.support import expected_conditions as EC
 naver_css = {
     'review_li': 'li.pui__X35jYm.EjjAW',
     'category_fold_a': 'a[data-pui-click-code="keywordmore"]',
-    'load_more_XPATH': '//*[@id="app-root"]/div/div/div/div[6]/div[2]/div[3]/div[2]/div/a'
+    'load_more_XPATH': '//*[@id="app-root"]/div/div/div/div[6]/div[2]/div[3]/div[2]/div/a',
+    'category_XPATH' : '//*[@id="app-root"]/div/div/div/div[6]/div[2]/div[3]/div[1]/ul/li/div[5]/span'
+
 }
 
 # URL
@@ -29,6 +31,25 @@ options.add_argument("disable-gpu")
 now = datetime.datetime.now()
 file_name = 'naver_review_' + now.strftime('%Y-%m-%d_%H-%M-%S') + '.json'
 
+# 날짜 형식 변환
+def time_formatter(posting_time):
+    try:
+        if re.match(r'^\d{1,2}\.\d{1,2}\.\w$', posting_time):
+            month, day = posting_time.split('.')[:2]
+            posting_time = datetime.datetime.strptime(f'2024.{month}.{day}', '%Y.%m.%d')
+        elif re.match(r'^\d{2}\.\d{1,2}\.\d{1,2}\.\w$', posting_time):
+            posting_time = datetime.datetime.strptime(posting_time[:8], '%y.%m.%d')
+        else:
+            posting_time = 'Invalid Date Format'
+
+        return posting_time
+
+    except ValueError as e:
+        print(f"Date format error for posting_time: {posting_time} - {e}")
+        posting_time = 'Invalid Date Format'
+
+        return posting_time
+
 # 크롤링 시작
 try:
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
@@ -38,7 +59,7 @@ try:
     # 페이지 다운
     driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
     try:
-        for i in range(15):
+        for i in range(1):
             element = driver.find_element(By.XPATH, naver_css['load_more_XPATH'])
             driver.execute_script("arguments[0].scrollIntoView(true);", element)
             time.sleep(1)  # 페이지가 완전히 로드되었는지 기다림
@@ -48,7 +69,8 @@ try:
     else:
         print('더보기 작업 종료')
 
-    time.sleep(25)
+    print("10초 sleep...")
+    time.sleep(10)
     html = driver.page_source
     bs = BeautifulSoup(html, 'lxml')
     reviews = bs.select(naver_css['review_li'])
@@ -77,50 +99,32 @@ try:
     for r in reviews:
         # content
         content = r.select_one('div.pui__vn15t2 > a.pui__xtsQN-')
+        content = content.text if content else ''
 
         # posting_time
         date_elements = r.select('div.pui__QKE5Pr > span.pui__gfuUIT > time')
         posting_time = date_elements[0] if date_elements else 'N/A'
+        posting_time = posting_time.text if posting_time else ''
+        posting_time = time_formatter(posting_time)
+        posting_time_str = posting_time.strftime('%Y-%m-%d %H:%M:%S') if isinstance(posting_time, datetime.datetime) else posting_time    # 문자열로 전환
+
 
         # category
-        category_span_elements = r.select('span.pui__jhpEyP')
-        print(category_span_elements)
+        category_span_elements = driver.find_elements(By.XPATH, naver_css['category_XPATH'])
+        for element in category_span_elements:
+            category = element.text
 
-        # 예외 처리
-        content = content.text if content else ''
-        posting_time = posting_time.text if posting_time else ''
+            review_data = {
+                "content_id": content_id,
+                "content": content,
+                "posting_time": posting_time_str,
+                "category": category
+            }
 
-        # 날짜 형식 변환
-        try:
-            if re.match(r'^\d{1,2}\.\d{1,2}\.\w$', posting_time):
-                month, day = posting_time.split('.')[:2]
-                posting_time = datetime.datetime.strptime(f'2024.{month}.{day}', '%Y.%m.%d')
-            elif re.match(r'^\d{2}\.\d{1,2}\.\d{1,2}\.\w$', posting_time):
-                posting_time = datetime.datetime.strptime(posting_time[:8], '%y.%m.%d')
-            else:
-                posting_time = 'Invalid Date Format'
-        except ValueError as e:
-            print(f"Date format error for posting_time: {posting_time} - {e}")
-            posting_time = 'Invalid Date Format'
+            reviews_list.append(review_data)
+            content_id += 1
+            time.sleep(0.06)
 
-        # 카테고리 항목을 모두 가져와서 JSON에 저장
-        categories = [category.text for category in category_span_elements] if category_span_elements else ['N/A']
-
-        # 날짜를 문자열로 변환
-        posting_time_str = posting_time.strftime('%Y-%m-%d %H:%M:%S') if isinstance(posting_time, datetime.datetime) else posting_time
-
-        # 리뷰 데이터를 dict 형태로 준비
-        review_data = {
-            "content_id": content_id,
-            "content": content,
-
-            "posting_time": posting_time_str,  # 날짜를 문자열로 변환하여 저장
-            "categories": categories  # 카테고리 리스트 추가
-        }
-
-        reviews_list.append(review_data)
-        content_id += 1
-        time.sleep(0.06)
 
     # 데이터를 JSON 파일로 저장
     with open(file_name, mode='w', encoding='utf-8') as json_file:
@@ -128,9 +132,13 @@ try:
 
     print(f"Data saved to {file_name}")
 
+
+
+# 에러 발생 시 빈 리스트 저장
 except Exception as e:
     print(e)
-    # 에러 발생 시 빈 리스트 저장
     with open(file_name, mode='w', encoding='utf-8') as json_file:
-        json.dump([], json_file, ensure_ascii=False, indent=4)  # 빈 리스트 저장
+        json.dump([], json_file, ensure_ascii=False, indent=4)
     print(f"Error occurred. Temporary file saved: {file_name}")
+
+
